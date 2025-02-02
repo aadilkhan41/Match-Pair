362f9c0671133875f8e5379d9ea9b1f64bb2a7b8
Aadil <aadilkhan3007@gmail.com>
2025-02-02 23:17:02 +0530

    Match Pair Update

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 const article = document.querySelector("article");
 let attempts = 0;
+const numbers = [];
+const images = ["strawberry.png","cherries.png","pineapple.png","lemon.png","pear.png","orange-slice.png","pomegranate.png","kiwi.png"];
+
+for (let i = 1; i <= 8; i++) {
+    numbers.push(i, i);
+}
+
+for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+}
+
+function renderCards(){
+    for(let i=0; i<numbers.length; i++){
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.set = numbers[i];
+        card.dataset.id = i;
+    
+        const cardInner = document.createElement('div');
+        cardInner.classList.add('card-inner');
+    
+        const cardFront = document.createElement('div');
+        cardFront.classList.add('card-front');
+    
+        const cardBack = document.createElement('div');
+        cardBack.classList.add('card-back');
+    
+        const img = document.createElement('img');
+        img.src = "./"+images[numbers[i]-1];
+    
+        cardBack.appendChild(img);
+    
+        cardInner.appendChild(cardFront);
+        cardInner.appendChild(cardBack);
+        card.appendChild(cardInner);
+    
+        article.appendChild(card);
+    }
+}
+
+renderCards();
+
 article.addEventListener("click",function(event){
     const sectionObj = event.target.closest(".card");
     sectionObj.classList.add("active");
